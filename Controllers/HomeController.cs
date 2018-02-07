f7632046e57d76fee09c7d1c34fbaba43faa7267
Tory16 <35581628+Tory16@users.noreply.github.com>
2018-02-07 10:08:30 -0600

    Completo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace Agua.Controllers
 {
     public class HomeController : Controller
     {
+        public DateTime TimeSpan { get; private set; }
+
         public IActionResult Index()
         {
             return View();
@@ -18,14 +20,22 @@ namespace Agua.Controllers
         
         public IActionResult Calcular(DateTime ProximoCumple, int minutos)
         {
+            int noches;
+            int Botella = 12;
+            int botellaxdia;
+            
+            DateTime Hoy =DateTime.Today;
+            TimeSpan resta = ProximoCumple - Hoy;
+            noches=resta.Days;
+            botellaxdia = minutos * resta.Days * Botella;
+
             /// Aquí tienen que hacer todo
             /// La fecha que y los minutos vienen del formulario 
             /// Investigar Model Binding 
 
 
-            ViewBag.Dias = 0;
-            ViewBag.Botellas = 0;
-
+            ViewBag.Dias = noches;
+            ViewBag.Botellas = botellaxdia;
             return View();
         }
 
